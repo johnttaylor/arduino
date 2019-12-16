c6e1656cf0749b35a8b263fc76b4358c8f2b2a9c
john <john.t.taylor@gmail.com>
2019-12-15 19:56:49 -0700

    wip

---
+++ src/Bsp/Adafruit/grand_central_m4/gcc/Api.cpp
@@ -0,0 +1,74 @@
+/*-----------------------------------------------------------------------------
+* This file is part of the Colony.Core Project.  The Colony.Core Project is an
+* open source project with a BSD type of licensing agreement.  See the license
+* agreement (license.txt) in the top/ directory or on the Internet at
+* http://integerfox.com/colony.core/license.txt
+*
+* Copyright (c) 2017  John T. Taylor
+*
+* Redistributions of the source code must retain the above copyright notice.
+*----------------------------------------------------------------------------*/
+
+#include "Bsp/Api.h"
+#include "Cpl/Io/Serial/Arduino/InputOutput.h"
+#include <ios>
+
+#define SECT_   "bsp"
+
+
+static Cpl::Io::Serial::Arduino::InputOutput fd_();
+extern Cpl::Io::InputOutput& Bsp_Serial( void );
+
+
+
+///////////////////////////////////////////
+void Bsp_Api_initialize( void )
+{
+    // Configure the LEDs as output pins 
+    pinMode( PIN_LED1, OUTPUT );
+    pinMode( PIN_LED2, OUTPUT );
+}
+
+void Bsp_beginArduinoSerialObject( unsigned long baudrate, uint16_t frameConfig )
+{
+    fd_.start( baudrate, frameConfig );
+    while ( fd_.isReady() == false )
+        ;
+}
+
+Cpl::Io::InputOutput& Bsp_Serial( void )
+{
+    return fd_;
+}
+
+
+
+////////////////////////////////////////////////////
+/*
+** Method stubs to satisfy the linker -->NOTE: C++ Streams are NOT supported by this BSP
+*/
+//void std::ios_base::Init::_S_ios_destroy()
+//{
+//}
+//
+//void std::ios_base::Init::_S_ios_create( bool  )
+//{
+//}
+
+std::ios_base::Init::Init()
+{
+}
+
+std::ios_base::Init::~Init()
+{
+}
+
+void *operator new(size_t size, std::nothrow_t const&){
+  return pvPortMalloc( size );
+}
+
+void *operator new[]( size_t size, std::nothrow_t const& ) {
+    return vPortFree( size );
+}
+
+const std::nothrow_t std::nothrow;
